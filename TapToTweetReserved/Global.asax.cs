a7323a2b5783fe240ef3a8cccd6df08002ff32f3
jsakamoto <junichi.jp@gmail.com>
2013-09-18 22:01:37 +0900

    Remove unreferenced libraries (ex. WebAPI).

--- TapToTweetReserved/Global.asax.cs
+++ TapToTweetReserved/Global.asax.cs
@@ -16,7 +16,6 @@ namespace TapToTweetReserved
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             AuthConfig.RegisterAuth();
