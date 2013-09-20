821dd3402033bbc52b3e77d88a9bf6e01679ae82
jsakamoto <junichi.jp@gmail.com>
2013-09-20 12:59:12 +0900

    Enhance: Add count characters feature.

---
+++ TapToTweetReserved/Scripts/charcounter.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    var MAXCHARS = 140;
+    var $textarea = $('textarea');
+    var $charcounter = $('#char-counter');
+
+    var updateCharCounter = function () {
+        var left = MAXCHARS - $textarea.val().length;
+        $charcounter
+            .text(left)
+            .toggleClass('overflow', left <= 0);
+    };
+
+    $textarea.on('keydown keyup keypress change click', updateCharCounter);
+    updateCharCounter();
+});
