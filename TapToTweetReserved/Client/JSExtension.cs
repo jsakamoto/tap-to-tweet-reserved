2b1f830a5a9cc60977eaa6b2e361088aedfd2f1c
jsakamoto <junichi.jp@gmail.com>
2020-01-01 13:09:41 +0900

    Improve initial focus behavior.

---
+++ TapToTweetReserved/Client/JSExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace TapToTweetReserved.Client
+{
+    public static class JSExtension
+    {
+        public static void SetInitialFocus(this IJSRuntime jSRuntime)
+        {
+            (jSRuntime as IJSInProcessRuntime)?.InvokeVoid("eval", "setTimeout(()=>(document.querySelector('*[autofocus]')||{focus:()=>{}}).focus(), 200)");
+        }
+    }
+}
