cbe5562f827f018fb23561ded6bb2bb79953205d
jsakamoto <junichi.jp@gmail.com>
2020-01-02 12:20:44 +0900

    Implement change languages feature.

---
+++ TapToTweetReserved/Client/SupportedLanguage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace TapToTweetReserved.Client
+{
+    public class SupportedLanguage
+    {
+        public readonly string Caption;
+
+        public readonly string LangCode;
+
+        public SupportedLanguage(string caption, string langCode)
+        {
+            Caption = caption;
+            LangCode = langCode;
+        }
+
+        public static readonly IReadOnlyList<SupportedLanguage> List = new[] {
+            new SupportedLanguage("English", "en"),
+            new SupportedLanguage("日本語", "ja"),
+        };
+    }
+}
